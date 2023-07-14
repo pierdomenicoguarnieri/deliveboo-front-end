dd069f61b68b68d96ea83fd38940c05c027a6d99
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-07-14 10:35:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Home from './components/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+}
+</script>
+
+<template>
+  <Home/>
+</template>
+
+<style lang="scss">
+  @import './scss/app.scss';
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<template>
+  <h1>Home</h1>
+</template>
+
+<style>
+
+</style>
